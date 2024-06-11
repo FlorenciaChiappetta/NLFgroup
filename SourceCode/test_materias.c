3d136b48feb6dee880d694b25ba849a37020b32a
Leandro Fajardo <fajardoleandro3@gmail.com>
2024-06-10 23:47:31 -0300

    Ultimo comit

---
+++ SourceCode/test_materias.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include "estructuras.h"
+// #include "estudiantes.h"
+
+
+// int main() {
+//     char nombreMateria[] = "Inteligencia Artificial";
+//     int anio = 3;
+
+//     Materia *nuevaMateria = crearMateria(nombreMateria, anio);
+
+//     printf("Materia creada: %s\n", nuevaMateria->nombre);
+
+//     free(nuevaMateria);
+//     return 0;
+// }
